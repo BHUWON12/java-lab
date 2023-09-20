d7d0977b914c63dcbffe890ac300ca97347c22d6
Bhuwan Singh  Bhandari <116028881+BHUWON12@users.noreply.github.com>
2023-09-20 11:29:53 +0530

    lab4.java

--- lab4.java
+++ lab4.java
@@ -1,33 +1,23 @@
-
-//Write a program to create a user defined exception say payoutOfBounds.
 import java.util.*;
-class payoutOfboundsException extends Exception
+class payoutOfBoundsException extends Exception
 {
-
-payoutOfBoundsException(String msg){
-
-    System.out.println("pay out of bounds exception"+msg);
-
-
-}
-
-
-}
-
-public class lab4
+payoutOfBoundsException(String msg)
 {
-
-    public static void main(String args[]) throws payoutOfBoundsException{
-System.out.println("Enter the employee's salary");
+System.out.println("pay out of bounds exception"+msg);
+}
+}
+public class UDEDemo
+{
+public static void main(String args[]) throws payoutOfBoundsException
+{
+System.out.println("Enter the employee salary:");
 Scanner sc=new Scanner(System.in);
 int pay=sc.nextInt();
-if(pay<10000||pay>50000)
+if(pay<10000 || pay>50000)
 {
-       throw new payoutOfBoundsException("salary not in valid range");
+throw new payoutOfBoundsException("salary not in a valid range");
 }
 else
-{
-    System.out.println("Employee is eligible for 30% hike");
+System.out.println("Employee is eligible for 30% hike");
 }
-
-}}
+}
